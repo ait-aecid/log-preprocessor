01254299b87b36fce3842ba28d7fb9f58cfe8186
viktorbeck98 <viktor.beck98@gmail.com>
2024-10-07 17:14:50 +0200

    minor changes

--- Data.py
+++ Data.py
@@ -14,24 +14,19 @@ class Data:
         data_dir: str,
         parser_name: str,
         default_timestamp_paths: str,
-        label_file_path: Optional[str] = None,
         tmp_save_path="tmp/current_data.log",
         parsed_data_dir="tmp/data_parsed/",
-        #data_file_paths: Optional[list] = None,
+        # data_file_paths: Optional[list] = None,
     ):
         self.data_dir = data_dir
-        #self.data_file_paths = data_file_paths
-        self.label_file_path = label_file_path
         self.parser_name = parser_name
         self.parser = Parser(parser_name, default_timestamp_paths)
         self.default_timestamp_paths = default_timestamp_paths
         self.tmp_save_path = tmp_save_path
         self.parsed_data_dir = parsed_data_dir
-        self.input_filepaths = self._get_input_filepaths(ordered=True)
+        self.n_lines, self.start_timestamps = self._get_logfiles_info_from_dir()
+        self.input_filepaths = self._get_input_filepaths(ordered=True) # order is crucial!!!
 
-    def get_labels(): #TO-DO
-        return
-    
     def get_df(self, use_parsed_data=True) -> pd.DataFrame:
         """Get the data as a single dataframe."""
         concatenate_files(self.input_filepaths, self.tmp_save_path) # concat files and save to tmp folder
@@ -52,11 +47,10 @@ class Data:
             df = self.parser.parse_file(self.tmp_save_path, None, True, "ts")
         return df
 
-    def get_logfiles_info_from_dir(self):
+    def _get_logfiles_info_from_dir(self):
         """Returns number of lines and starting time of log files."""
         root, dirs, files = list(os.walk(self.data_dir))[0]
         n_lines = {}
-        # get files sorted by start times of data files
         start_timestamps = {}
         for file in files:
             path = os.path.join(self.data_dir, file)
@@ -72,9 +66,7 @@ class Data:
 
     def _get_input_filepaths(self, ordered=False):
         """Return input filespaths."""
-        root, dirs, files = list(os.walk(self.data_dir))[0]
         if ordered:
-            n_lines, start_timestamps = self.get_logfiles_info_from_dir()
-            files = list(dict(sorted(start_timestamps.items(), key=lambda x: x[1])).keys()) # sort files
+            files = list(dict(sorted(self.start_timestamps.items(), key=lambda x: x[1])).keys()) # sort files
         self.input_filepaths = [os.path.join(self.data_dir, file) for file in files]
         return self.input_filepaths
